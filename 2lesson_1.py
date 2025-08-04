c96b96ffcc16c963b8f334f57bdda2aa27e4c9c0
venera081 <berekettop0010.@gmail.com>
2025-08-04 15:29:19 +0600

    homework1

---
+++ 2lesson_1.py
@@ -0,0 +1,15 @@
+# OOП 1: Основы ООП, Создание первых классов, Атрибуты и Методы классов,
+# Принцип ООП - Наследование.
+
+class Car:
+    def __init__(self, max_speed, color, model):
+        self.max_speed = max_speed
+        self.color = color
+        self.model = model
+
+car_honda = Car(160, "silver", "Honda Fit")
+car_subaru = Car(180, "black", "Subaru Fit")
+print(car_honda.max_speed, car_honda.color, car_honda.model)
+print(car_subaru.max_speed, car_subaru.color, car_subaru.model)
+
+
---
+++ homework_1.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, name, birth_date, occupation, higher_education):
+        self.name = name
+        self.birth_date = birth_date
+        self.occupation = occupation
+        self.higher_education = higher_education
+
+person_1 = Person("venera", 8.11, "student", False )
+person_2 = Person("vika", 15.9, "student", False)
+person_3 = Person("asema", 5.1, "model", False)
+print(f"1)Имя: {person_1.name}, дата рождения: {person_1.birth_date},"
+      f" профессия: {person_1.occupation}, высшее образование: {person_1.higher_education}")
+print(f"2)Имя: {person_2.name}, дата рождения: {person_2.birth_date},"
+      f" профессия: {person_2.occupation}, высшее образование: {person_2.higher_education}")
+print(f"3)Имя: {person_3.name}, дата рождения: {person_3.birth_date},"
+      f" профессия: {person_3.occupation}, высшее образование: {person_3.higher_education}")
