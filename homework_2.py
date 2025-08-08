fcdb992f52b3ad767b53ab61d21ca788d1819ef9
venera081 <berekettop0010.@gmail.com>
2025-08-08 13:00:30 +0600

    Поставила пробелы

--- homework_2.py
+++ homework_2.py
@@ -4,19 +4,24 @@ class Person:
         self.birth_date = birth_date
         self.occupation = occupation
         self.higher_education = higher_education
+
     def introduce(self):
         print(f"Меня зовут {person.name}, я родилась {self.birth_date} и я {self.occupation} ")
+
 class Friend(Person):
     def __init__(self, name, birth_date, occupation, higher_education, hobby):
         super().__init__(name, birth_date, occupation, higher_education)
         self.hobby = hobby
+
     def introduce(self):
         print(f"Привет, меня зовут {self.name}, я подруга {person_1.name}, и мы {self.hobby}, "
               f"я родилась {self.birth_date}, я {self.occupation}")
+
 class Classmate(Person):
     def __init__(self, name, birth_date, occupation, higher_education, group_name):
         super().__init__(name, birth_date, occupation, higher_education)
         self.group_name = group_name
+
     def introduce(self):
         print(f"Привет, меня зовут {self.name}, я одноклассница {person_1.name},"
               f" учились вместе в {self.group_name}, я родилась {self.birth_date}, я {self.occupation} ")
@@ -31,6 +36,7 @@ friend = Friend("Асема", "5.01.2009", "модель", False, "любим о
                                                        "музыкальную группу")
 for people in [classmate, classmate2, friend]:
     people.introduce()
+    
 
 
 
