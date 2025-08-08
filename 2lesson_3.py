c1e094ec996fa688f31ebae816ecfb34b6525c35
venera081 <berekettop0010.@gmail.com>
2025-08-08 18:57:05 +0600

    Прошли абстракцию и инкапсуляцию

--- 2lesson_3.py
+++ 2lesson_3.py
@@ -6,7 +6,7 @@ class Car:
         self.max_speed = max_speed
         self.color = color
         self.model = model
-        self.__fined = True
+        self.__fined = False
 
     def pay_fine(self):
         # setter
@@ -17,15 +17,53 @@ class Car:
         # getter
         return self.__fined
 
-    def drive_to_location(self, location):
-        if self.__fined:
-            print(f"Car {self.model} is fined and can not drive everywhere")
-        else:
-            print(f"Car {self.model} is driving to {location}")
+    # def drive_to_location(self, location):
+    #     if self.__fined:
+    #         raise ValueError(f"Car {self.model} is fined and can not drive everywhere")
+    #     print(f"Car {self.model} is driving to {location}")
 
     def __test(self):
         print("Private method")
 
+    @property
+    def fined(self):
+        return self.__fined
+
+    @fined.setter
+    def fined(self, value):
+        # setter
+        self.__fined = value
+
+class Truck(Car):
+    def test(self):
+        print(f"Truck test - {self.get_fined()}")
+
 car_honda = Car(140, "silver", "Honda Fit")
-print(car_honda.drive_to_location("Karakol"))
+# car_honda.drive_to_location("Karakol")
 print(car_honda.get_fined())
+print(car_honda.fined)
+car_honda.fined = True
+# car_honda.drive_to_location("Karakol")
+truck_man = Truck(150, "grey", "MAN")
+truck_man.test()
+# print(car_honda._Car__fined)   так лучше не делать
+
+
+
+
+# Абстракция
+
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def make_sound(self):
+        pass
+
+class Dog(Animal):
+    def make_sound(self):
+        print("гав гав")
+
+puppy = Dog()
+puppy.make_sound()
