1f2f4747dd5c9cf25bfade5e433bf58bac6863b5
venera081 <berekettop0010.@gmail.com>
2025-08-18 20:53:56 +0600

    Изменила название файла

--- homeworks/homework_5.py
+++ homeworks/homework_5.py
@@ -1,20 +1,8 @@
 from homeworks.distance import Distance
 
 
-def main():
-    distan1 = Distance(25, "см")
-    distan2 = Distance(50, "км")
-    distan3 = Distance(90, "м")
-    distan4 = Distance(85, "мм")
 
 
-
-
-
-
-
-if __name__ == "__main__":
-    main()
 
 
 
