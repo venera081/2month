f3dfa93c65352497e20b7c09580751587187e771
venera081 <berekettop0010.@gmail.com>
2025-08-21 15:04:43 +0600

    Сделала дз и создала файл requirements.txt

---
+++ homeworks/homework_6.py
@@ -0,0 +1,11 @@
+from blessed import Terminal
+
+term = Terminal()
+
+print(term.red + "🍎apple🍎" + term.normal)
+print(term.olivedrab3 + "🍌banana🍌" + term.normal)
+print(term.navajowhite + "🍒cherry🍒" + term.normal)
+print(term.steelblue3 + "🍇grape🍇" + term.normal)
+print(term.mediumorchid + "🥭mango🥭" + term.normal)
+print(term.darkturquoise + "🍊orange🍊" + term.normal)
+print(term.lavenderblush2 + "🍑peach🍑" + term.normal)
