e8334422bcb310028ff1ecf7302a5f453c6b9202
venera081 <berekettop0010.@gmail.com>
2025-08-22 16:49:56 +0600

    Проверила домашку номер 5

--- homeworks/homework_5.py
+++ homeworks/homework_5.py
@@ -18,15 +18,15 @@ print(f"Вычитание:\n  "
       f"{dt_1} - {dt_3} = {dt_1 - dt_3}\n "
       f"{dt_4} - {dt_2} = {dt_4 - dt_2}\n "
       f"{dt_4} - {dt_3} = {dt_4 - dt_3}")
-try:
-    print(f"{dt_4} - {dt_1} = {dt_4 - dt_1}")
-    print(f"{dt_3} - {dt_2} = {dt_3 - dt_2}")
-    print(f"{dt_2} - {dt_1} = {dt_2 - dt_1}")
-    print(f"{dt_3} - {dt_1} = {dt_3 - dt_1}")
-    print(f"{dt_2} - {dt_4} = {dt_2 - dt_4}")
-    print(f"{dt_3} - {dt_4} = {dt_3 - dt_4}")
-except ValueError:
-    print("Произошла ошибка, ответ дает отрицательное число")
+# try:
+print(f"{dt_4} - {dt_1} = {dt_4 - dt_1}")
+#     print(f"{dt_3} - {dt_2} = {dt_3 - dt_2}")
+#     print(f"{dt_2} - {dt_1} = {dt_2 - dt_1}")
+#     print(f"{dt_3} - {dt_1} = {dt_3 - dt_1}")
+#     print(f"{dt_2} - {dt_4} = {dt_2 - dt_4}")
+#     print(f"{dt_3} - {dt_4} = {dt_3 - dt_4}")
+# except ValueError:
+#     print("Произошла ошибка, ответ дает отрицательное число")
 
 print(f"Сравнение:\n  "
       f"{dt_1} == {dt_3} -> {dt_1 == dt_3}\n "
